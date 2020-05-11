fdc7494b57c0b4744c3ff9b2f1e44f9406a7b364
tommasoviciani <49819683+tommasoviciani@users.noreply.github.com>
2020-05-11 12:11:11 +0200

    La classe PancakeBlock estende CakeBlock. Modifiche al modello JSON di PancakeBlock

--- src/main/java/com/example/examplemod/blocks/PancakeBlock.java
+++ src/main/java/com/example/examplemod/blocks/PancakeBlock.java
@@ -1,14 +1,15 @@
 package com.example.examplemod.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.CakeBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
-public class PancakeBlock extends Block {
+public class PancakeBlock extends CakeBlock {
 
     public PancakeBlock() {
         super(Block.Properties.create(Material.CAKE)
         .hardnessAndResistance(0.5f, 0.5f)
-        .sound(SoundType.SNOW));
+        .sound(SoundType.CLOTH));
     }
 }
